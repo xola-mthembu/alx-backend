364a49d9f7fa9fbbb587094f53cd148b8e9bf18e
xola-mthembu <xola.mthembu@gmail.com>
2024-08-20 17:11:12 +0000

    Task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" MRUCache module """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache defines a MRU caching system """
+
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = next(reversed(self.cache_data))
+                print(f"DISCARD: {last_key}")
+                self.cache_data.pop(last_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key, None)
