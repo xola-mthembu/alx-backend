94d4ff67a3c20686cf5dbf7cafa79e4e8895b942
xola-mthembu <xola.mthembu@gmail.com>
2024-08-27 22:30:10 +0000

    8. Display the current time

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Display the current time"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+import pytz
+from datetime import datetime
+
+
+class Config:
+    """Config class for Flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Returns a user dictionary or None if ID not found"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """Find a user if any, and set it as a global on flask.g.user"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match with our supported languages"""
+    # 1. Locale from URL parameters
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+
+    # 2. Locale from user settings
+    if g.user and g.user['locale'] in app.config['LANGUAGES']:
+        return g.user['locale']
+
+    # 3. Locale from request header
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    """Infer appropriate time zone"""
+    # 1. Find timezone parameter in URL parameters
+    timezone = request.args.get('timezone')
+    if timezone:
+        try:
+            pytz.timezone(timezone)
+            return timezone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    # 2. Find time zone from user settings
+    if g.user and g.user['timezone']:
+        try:
+            pytz.timezone(g.user['timezone'])
+            return g.user['timezone']
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+
+    # 3. Default to UTC
+    return "UTC"
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Render the index page"""
+    timezone = get_timezone()
+    tz = pytz.timezone(timezone)
+    current_time = datetime.now(tz)
+    formatted_time = current_time.strftime("%b %d, %Y, %I:%M:%S %p")
+    return render_template('index.html', current_time=formatted_time)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
