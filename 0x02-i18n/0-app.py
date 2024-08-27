11daba958ed9c3e2c9d9b42c3f87c38330d9fc7d
xola-mthembu <xola.mthembu@gmail.com>
2024-08-27 19:11:08 +0000

    0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-"""
-Basic Flask app module
-"""
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """Return the index page."""
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Render the index page"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="5000")
