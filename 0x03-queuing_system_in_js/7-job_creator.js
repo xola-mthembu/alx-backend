699802c3f92722b18b46a7b628857e1f53bddae6
xola-mthembu <xola.mthembu@gmail.com>
2024-09-23 14:27:04 -0700

    Task 7: Kue job creator and processor with array

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  }
+];
+
+jobs.forEach((jobData) => {
+  const job = queue.create('push_notification_code_2', jobData).save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    }
+  });
+
+  job.on('complete', () => {
+    console.log(`Notification job ${job.id} completed`);
+  });
+
+  job.on('failed', (errorMessage) => {
+    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
+  });
+
+  job.on('progress', (progress) => {
+    console.log(`Notification job ${job.id} ${progress}% complete`);
+  });
+});
