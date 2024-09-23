825d72c42158e81ee80d48c0e3fc8159ce111dd8
xola-mthembu <xola.mthembu@gmail.com>
2024-09-23 14:25:43 -0700

    Task 6: Kue job creator and processor

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(
+    `Sending notification to ${phoneNumber}, with message: ${message}`
+  );
+}
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
